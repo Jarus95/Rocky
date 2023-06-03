4d2cd8154df3da360356a48c27a1e60519dd2976
Jarus95 <aliym20199@gmail.com>
2023-06-03 11:59:14 +0500

    Category controller

---
+++ Rocky/Controllers/CategoryController.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Rocky.Data;
+
+namespace Rocky.Controllers
+{
+    public class CategoryController : Controller
+    {
+        private readonly ApplicationDbContext dbContext;
+
+        public CategoryController(ApplicationDbContext _dbContext)
+        {
+             dbContext = _dbContext;
+        }
+
+        public IActionResult Index()
+        {
+            var catergories = dbContext.Category.ToList();
+            return View(catergories);
+        }
+    }
+}
