40fcd61f80816e4ecbcff791aac3bcd3f1ba89a9
Jarus95 <aliym20199@gmail.com>
2023-06-08 07:55:16 +0500

    fssf

---
+++ Rocky/Controllers/ProductController.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Rocky.Data;
+
+namespace Rocky.Controllers
+{
+    public class ProductController : Controller
+    {
+        private readonly ApplicationDbContext dbContext;
+
+        public ProductController(ApplicationDbContext _dbContext)
+        {
+            dbContext = _dbContext;
+        }
+
+
+        public IActionResult Index()
+        {
+            var product = dbContext.Product.ToList();
+
+            foreach (var item in product)
+            {
+                item.Category = dbContext.Category.FirstOrDefault(p=>p.Id == item.CategoryId);
+            }
+
+            return View(product);
+        }
+    }
+}
