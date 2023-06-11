607e1f0db10b12239413d4471e3c7b2f7a93a61e
Jasur Sulaymonov <kinobor2016@gmail.com>
2023-06-11 16:33:27 +0300

    def

--- Rocky/Controllers/ApplicationTypeController.cs
+++ Rocky/Controllers/ApplicationTypeController.cs
@@ -8,6 +8,7 @@ namespace Rocky.Controllers
     {
         public readonly  ApplicationDbContext dbContext;
 
+
         public ApplicationTypeController(ApplicationDbContext applicationDb)
         {
             dbContext = applicationDb;
